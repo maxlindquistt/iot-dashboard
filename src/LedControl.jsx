ff8ac12dbed1805b6ad9fb2455f346e46ecdb088
Max Lindquist <max.lindquist312@gmail.com>
2026-05-27 17:09:11 +0200

    Update API endpoint in LedControl component to production URL

--- src/LedControl.jsx
+++ src/LedControl.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-const API = 'http://localhost:3000/api';
+const API = 'https://iot-backend-production-b5f0.up.railway.app/api';
 
 export default function LedControl() {
     const [ledOn, setLedOn] = useState(false);
